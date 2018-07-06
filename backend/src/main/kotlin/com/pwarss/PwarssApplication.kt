d6d3ec287f7762812f382a590b57429cd04dd494
Khvan Konstantin <748538+KhvanKonstantin@users.noreply.github.com>
2018-07-06 15:46:12 +0300

    Initialized spring boot project with gradle build

---
+++ backend/src/main/kotlin/com/pwarss/PwarssApplication.kt
@@ -0,0 +1,12 @@
+// Created by Konstantin Khvan on 7/6/18 11:05 AM
+package com.pwarss
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PwarssApplication
+
+fun main(args: Array<String>) {
+    runApplication<PwarssApplication>(*args)
+}
