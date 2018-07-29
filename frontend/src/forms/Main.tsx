45b1443498af8fde4b71a42613275abb56fa9243
Khvan Konstantin <748538+KhvanKonstantin@users.noreply.github.com>
2018-07-29 15:11:43 +0300

    material like styling done with scss. header, menus and hover effects.

--- frontend/src/forms/Main.tsx
+++ frontend/src/forms/Main.tsx
@@ -2,7 +2,6 @@
 
 
 import * as React from 'react';
-import {FormEvent} from 'react';
 import NewsEntryList from "./NewsList";
 import SingleNewsEntry from "./SingleNewsEntry";
 import {inject} from "mobx-react/custom";
@@ -15,71 +14,98 @@ export interface RootProps {
 
 interface RootState {
     newsEntryId: number | null;
+    showLeftMenu: boolean;
+    showRightMenu: boolean;
 }
+
+const hideAllMenus = {showLeftMenu: false, showRightMenu: false};
 
 @inject("newsStore")
 export default class Main extends React.Component<RootProps, RootState> {
     state = {
-        newsEntryId: null
+        newsEntryId: null,
+        showLeftMenu: false,
+        showRightMenu: false
     };
 
-    private logout = (e: FormEvent) => {
-        e.preventDefault();
+    private logout = () => {
         this.props.doLogout();
     };
 
-    private back = (e: FormEvent) => {
-        e.preventDefault();
-        this.setState({newsEntryId: null})
+    private back = () => {
+        this.setState({...hideAllMenus, newsEntryId: null})
     };
 
-    private onTitleClicked = (id: number) => {
-        console.log(`Title clicked ${id}`);
+    private toggleLeftMenu = () => {
+        this.setState(prevState => ({...hideAllMenus, showLeftMenu: !prevState.showLeftMenu}));
+    };
+
+    private toggleRightMenu = () => {
+        this.setState(prevState => ({...hideAllMenus, showRightMenu: !prevState.showRightMenu}));
+    };
+
+    private showNewsEntry = (id: number) => {
         const newsStore = this.props.newsStore!;
         newsStore.markEntryRead(id, true);
-        this.setState({newsEntryId: id});
+        this.setState({...hideAllMenus, newsEntryId: id});
     };
 
-    private onMarkUnreadClicked = (id: number) => {
-        console.log(`Mark unread clicked ${id}`);
+    // @ts-ignore
+    private markEntryRead = (id: number, read: boolean) => {
         const newsStore = this.props.newsStore!;
-        newsStore.markEntryRead(id, false);
+        newsStore.markEntryRead(id, read);
     };
 
-    private onMarkClicked = (id: number) => {
-        console.log(`Mark clicked ${id}`);
+    private markEntry = (id: number) => {
         const newsStore = this.props.newsStore!;
         newsStore.toggleEntryMark(id);
+    };
+
+    private doHideAllMenus = () => {
+        console.log("on down");
+        this.setState(hideAllMenus);
     };
 
     render() {
         let content = null;
         const newsEntryId: number | null = this.state.newsEntryId;
+        const showEntry = newsEntryId != null;
+
         if (newsEntryId != null) {
             const newsStore = this.props.newsStore!;
-            content = <div>
-                <button onClick={this.back}>Back</button>
-                <SingleNewsEntry entry={newsStore.entryById(newsEntryId)}
-                                 onMarkClicked={() => this.onMarkClicked(newsEntryId)}
-                                 onMarkUnreadClicked={() => this.onMarkUnreadClicked(newsEntryId)}/>
-            </div>
+            content = <SingleNewsEntry entry={newsStore.entryById(newsEntryId)}
+                                       onMarkClicked={this.markEntry}/>
         } else {
-            content = <NewsEntryList onMarkClicked={this.onMarkClicked} onTitleClicked={this.onTitleClicked}/>;
+            content = <NewsEntryList onMarkClicked={this.markEntry} onTitleClicked={this.showNewsEntry}/>;
         }
+
+        const {showLeftMenu, showRightMenu} = this.state;
+
+        const topLeftButton = showEntry
+            ? <div className="item" onClick={this.back}><b>←</b></div>
+            : <div className="item">
+                <div onClick={this.toggleLeftMenu}>☰</div>
+                <div className={`menu ${showLeftMenu ? "active" : ""}`}>
+                    <div className="menu-item">Unread</div>
+                    <div className="menu-item">All entries</div>
+                    <div className="menu-item">Marked</div>
+                    <div className="menu-item" onClick={this.logout}>Logout</div>
+                </div>
+            </div>;
 
         return (<div className="main-form">
                 <div className="header">
-                    <div className="item menu">☰
-                        <div className="menu-item">☰</div>
-                        <div className="menu-item"><a onClick={this.logout}>Logout</a></div>
-                    </div>
+                    {topLeftButton}
                     <div className="item title"/>
-                    <div className="item right-menu">
-                        <div className="menu-item">⋮</div>
+                    <div className="item">
+                        <div onClick={this.toggleRightMenu}>⋮</div>
+                        <div className={`menu right ${showRightMenu ? "active" : ""}`}>
+                            {!showEntry && <div className="menu-item">Mark all read</div>}
+                            {showEntry && <div className="menu-item">Mark unread</div>}
+                        </div>
                     </div>
-
                 </div>
-                <div className="content">
+                <div className="content" onMouseDown={this.doHideAllMenus} onTouchStart={this.doHideAllMenus}>
                     {content}
                 </div>
             </div>
--- frontend/src/forms/SingleNewsEntry.tsx
+++ frontend/src/forms/SingleNewsEntry.tsx
@@ -1,7 +1,5 @@
 // Created by Konstantin Khvan on 7/23/18 6:00 PM
 
-// Created by Konstantin Khvan on 7/23/18 2:42 PM
-
 import * as React from "react";
 import {NewsEntry} from "../model/NewsEntry";
 import {extractTextFromHtmlString} from "./util";
@@ -10,17 +8,15 @@ import {observer} from "mobx-react";
 export interface SingleNewsEntryProps {
     entry: NewsEntry
     onMarkClicked: (id: number) => any
-    onMarkUnreadClicked: (id: number) => any
 }
 
 @observer
 class SingleNewsEntry extends React.Component<SingleNewsEntryProps> {
     render() {
-        const {entry, onMarkClicked, onMarkUnreadClicked} = this.props;
+        const {entry, onMarkClicked} = this.props;
 
-        const {id, title, marked, read, link, content, date} = entry;
+        const {id, title, marked, link, content, date} = entry;
 
-        const readCN = read ? "" : "unread";
         const markCN = !marked ? "notmarked" : "marked";
 
         const textContent = extractTextFromHtmlString(content);
@@ -30,7 +26,6 @@ class SingleNewsEntry extends React.Component<SingleNewsEntryProps> {
                 <div className={markCN} onClick={() => onMarkClicked(id)}>★</div>
                 <a rel="noopener noreferrer" target="_blank" className="title" href={link}>{title}</a>
             </div>
-            <div className={readCN} onClick={() => onMarkUnreadClicked(id)}>{!read ? "Mark unread" : "Mark read"}</div>
             <div className="date">{date}</div>
             <div className="content">{textContent}</div>
         </div>;
