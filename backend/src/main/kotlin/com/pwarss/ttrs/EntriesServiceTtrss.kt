319b865ede91c573014a792f3d8cbb630e4e42a4
Khvan Konstantin <748538+KhvanKonstantin@users.noreply.github.com>
2018-08-16 14:59:47 +0300

    user NamedParameterJdbcTemplate for db access

--- backend/src/main/kotlin/com/pwarss/ttrs/EntriesServiceTtrss.kt
+++ backend/src/main/kotlin/com/pwarss/ttrs/EntriesServiceTtrss.kt
@@ -4,16 +4,18 @@ package com.pwarss.ttrs
 
 import com.pwarss.PwaRssProperties
 import com.pwarss.model.NewsEntry
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.sql.Types
 
 /**
  * tt-rss db based entries repository
  */
 @Service
-class EntriesServiceTtrss(private val jdbcTemplate: JdbcTemplate, config: PwaRssProperties) {
+class EntriesServiceTtrss(private val jdbcTemplate: NamedParameterJdbcTemplate, config: PwaRssProperties) {
 
     val maxEntries = config.maxEntriesForRequest
 
@@ -28,89 +30,31 @@ class EntriesServiceTtrss(private val jdbcTemplate: JdbcTemplate, config: PwaRss
     }
 
     fun findEntryById(ownerId: Long, entryId: Long): NewsEntry? {
-
-        val query = """SELECT e.id           AS id,
-                              ue.unread      AS unread,
-                              ue.marked      AS marked,
-                              e.link         AS link,
-                              e.title        AS title,
-                              e.date_entered AS date,
-                              e.content      AS content
-                       FROM ttrss_user_entries AS ue
-                              JOIN ttrss_entries AS e ON ue.ref_id = e.id
-                       WHERE ue.owner_uid = ?
-                         AND ue.ref_id = ?"""
-
-        return jdbcTemplate.queryForObject(query, newsEntryMapper, ownerId, entryId)
+        return findEntriesImpl(ownerId, 1, entryId = entryId).firstOrNull()
     }
 
     fun findEntries(ownerId: Long, limit: Int): List<NewsEntry> {
-        val limit = limit.coerceIn(10, maxEntries)
-
-        val query = """SELECT e.id           AS id,
-                              ue.unread      AS unread,
-                              ue.marked      AS marked,
-                              e.link         AS link,
-                              e.title        AS title,
-                              e.date_entered AS date,
-                              e.content      AS content
-                       FROM ttrss_user_entries AS ue
-                              JOIN ttrss_entries AS e ON ue.ref_id = e.id
-                       WHERE ue.owner_uid = ?
-                       ORDER BY date_entered DESC, id DESC
-                       LIMIT ?"""
-
-        return jdbcTemplate.query(query, newsEntryMapper, ownerId, limit)
+        return findEntriesImpl(ownerId, limit)
     }
 
     fun findUnread(ownerId: Long, limit: Int): List<NewsEntry> {
-        val limit = limit.coerceIn(10, maxEntries)
-
-        val query = """SELECT e.id           AS id,
-                              ue.unread      AS unread,
-                              ue.marked      AS marked,
-                              e.link         AS link,
-                              e.title        AS title,
-                              e.date_entered AS date,
-                              e.content      AS content
-                       FROM ttrss_user_entries AS ue
-                              JOIN ttrss_entries AS e ON ue.ref_id = e.id
-                       WHERE ue.owner_uid = ?
-                         AND ue.unread = TRUE
-                       ORDER BY date_entered DESC, id DESC
-                       LIMIT ?"""
-
-        return jdbcTemplate.query(query, newsEntryMapper, ownerId, limit)
+        return findEntriesImpl(ownerId, limit, unread = true)
     }
 
     fun findStarred(ownerId: Long, limit: Int): List<NewsEntry> {
-        val limit = limit.coerceIn(10, maxEntries)
-
-        val query = """SELECT e.id           AS id,
-                              ue.unread      AS unread,
-                              ue.marked      AS marked,
-                              e.link         AS link,
-                              e.title        AS title,
-                              e.date_entered AS date,
-                              e.content      AS content
-                       FROM ttrss_user_entries AS ue
-                              JOIN ttrss_entries AS e ON ue.ref_id = e.id
-                       WHERE ue.owner_uid = ?
-                         AND ue.marked = TRUE
-                       ORDER BY date_entered DESC, id DESC
-                       LIMIT ?"""
-
-        return jdbcTemplate.query(query, newsEntryMapper, ownerId, limit)
+        return findEntriesImpl(ownerId, limit, marked = true)
     }
 
     @Transactional
     fun starEntry(ownerId: Long, entryId: Long, star: Boolean): Pair<Boolean, NewsEntry?> {
         val query = """UPDATE ttrss_user_entries
-                       SET marked = ?
-                       WHERE owner_uid = ?
-                         AND ref_id = ?"""
+                       SET marked = :marked
+                       WHERE owner_uid = :ownerId
+                         AND ref_id = :refId"""
 
-        val success = jdbcTemplate.update(query, star, ownerId, entryId) > 0
+        val args = mutableMapOf("ownerId" to ownerId, "refId" to entryId,
+                "marked" to star)
+        val success = jdbcTemplate.update(query, args) > 0
         val entry = findEntryById(ownerId, entryId)
 
         return success to entry
@@ -119,11 +63,14 @@ class EntriesServiceTtrss(private val jdbcTemplate: JdbcTemplate, config: PwaRss
     @Transactional
     fun readEntry(ownerId: Long, entryId: Long, read: Boolean): Pair<Boolean, NewsEntry?> {
         val query = """UPDATE ttrss_user_entries
-                       SET unread = ?
-                       WHERE owner_uid = ?
-                         AND ref_id = ?"""
+                       SET unread = :unread
+                       WHERE owner_uid = :ownerId
+                         AND ref_id = :refId"""
 
-        val success = jdbcTemplate.update(query, !read, ownerId, entryId) > 0
+        val args = mutableMapOf("ownerId" to ownerId, "refId" to entryId,
+                "unread" to !read)
+
+        val success = jdbcTemplate.update(query, args) > 0
         val entry = findEntryById(ownerId, entryId)
 
         return success to entry
@@ -132,9 +79,47 @@ class EntriesServiceTtrss(private val jdbcTemplate: JdbcTemplate, config: PwaRss
     fun readAll(ownerId: Long, maxId: Long): Boolean {
         val query = """UPDATE ttrss_user_entries
                        SET unread = FALSE
-                       WHERE owner_uid = ?
-                         AND ref_id <= ?"""
+                       WHERE owner_uid = :ownerId
+                         AND ref_id <= :refId"""
 
-        return jdbcTemplate.update(query, ownerId, maxId) > 0
+        val args = mutableMapOf("ownerId" to ownerId, "refId" to maxId)
+
+        return jdbcTemplate.update(query, args) > 0
+    }
+
+    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+    fun findEntriesImpl(ownerId: Long, limit: Int, entryId: Long? = null,
+                        minId: Long? = null, maxId: Long? = null,
+                        unread: Boolean? = null, marked: Boolean? = null): List<NewsEntry> {
+
+        val args = MapSqlParameterSource().apply {
+            addValue("ownerId", ownerId, Types.INTEGER)
+            addValue("limit", limit.coerceIn(10, maxEntries), Types.INTEGER)
+            addValue("unread", unread, Types.BOOLEAN)
+            addValue("marked", marked, Types.BOOLEAN)
+            addValue("minId", minId, Types.INTEGER)
+            addValue("maxId", maxId, Types.INTEGER)
+            addValue("refId", entryId, Types.INTEGER)
+        }
+
+        val query = """SELECT e.id           AS id,
+                              ue.unread      AS unread,
+                              ue.marked      AS marked,
+                              e.link         AS link,
+                              e.title        AS title,
+                              e.date_entered AS date,
+                              e.content      AS content
+                       FROM ttrss_user_entries AS ue
+                           JOIN ttrss_entries AS e ON ue.ref_id = e.id
+                       WHERE ue.owner_uid = :ownerId
+                           AND (:refId IS NULL OR ue.ref_id = :refId)
+                           AND (:unread IS NULL OR ue.unread = :unread)
+                           AND (:marked IS NULL OR ue.marked = :marked)
+                           AND (:minId IS NULL OR e.id > :minId)
+                           AND (:maxId IS NULL OR e.id < :maxId)
+                       ORDER BY id DESC, date_entered DESC
+                       LIMIT :limit"""
+
+        return jdbcTemplate.query(query, args, newsEntryMapper)
     }
 }
